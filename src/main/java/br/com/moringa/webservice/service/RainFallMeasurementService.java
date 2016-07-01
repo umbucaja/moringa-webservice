63637ce5d4e9d189020f8aeae175ecd9b3bad893
vstluan <luizantonio.ccf@gmail.com>
2016-07-01 15:55:54 -0300

    criacao de log

--- src/main/java/br/com/moringa/webservice/service/RainFallMeasurementService.java
+++ src/main/java/br/com/moringa/webservice/service/RainFallMeasurementService.java
@@ -2,6 +2,8 @@ package br.com.moringa.webservice.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,8 @@ public class RainFallMeasurementService {
     @Autowired
     MeasurementStationRepository msRepository;
     
+    private static final Logger LOG = LoggerFactory.getLogger(RainFallMeasurementService.class);
+    
     public List<RainFallMeasurementDomain> findAll(){
     	
         List<RainFallMeasurement> rains = rfmRepository.findAll();
@@ -33,7 +37,20 @@ public class RainFallMeasurementService {
 
         for (Station station : stationList) {
 
+        	if(station.getName().contains("/")){
+        		String[] temp = station.getName().split("/");
+        		station.setName(temp[temp.length - 1]);
+        	}
+        			
+        			
         	MeasurementStation ms = msRepository.findByName(station.getName());
+        	
+        	if(null == ms){
+        		LOG.info("********************************");
+        		LOG.info("Station :" + station.getName());
+        		LOG.info("List of Measurements :" + station.getWsmDomainList().toString());
+        		LOG.info("********************************");
+        	}
         	
             for(RainFallMeasurementDomain domain: station.getWsmDomainList()){
 
